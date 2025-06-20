991f887af5ad97cfba63294e4c3a69f11461f0f5
Andrea Ranzato <andrea@nutrivetpet.com>
2025-06-20 14:41:37 +0200

    feat: list prices endpoint

---
+++ R/e_prices.R
@@ -0,0 +1,40 @@
+#' Fetch Prices from Stripe API
+#'
+#' Retrieves prices data from the Stripe API.
+#'
+#' @inheritParams list_balance_transactions
+#'
+#' @return A data frame (tibble if available) containing prices data.
+#'
+#' @section API Documentation: For more information about Stripe balance
+#'   transactions, see: \url{https://docs.stripe.com/api/prices/}
+#'
+#' @examples
+#' \dontrun{
+#' # Fetch test mode balance transactions
+#' test_prices <- list_prices("test")
+#'
+#' # Fetch live mode balance transactions
+#' live_prices <- list_prices("live")
+#' }
+#'
+#' @export
+list_prices <- function(mode = c("test", "live"), limit = 10L) {
+  check_mode(mode)
+  check_limit(limit)
+
+  dat <- exec_api_call("prices", mode, limit)
+
+  cols <- get_cols("prices")
+  check_missing_cols(colnames(dat), cols)
+
+  dat[["unit_amount"]] <- convert_amt_to_decimal(dat[["unit_amount"]])
+  dat[["unit_amount_decimal"]] <- convert_amt_to_decimal(as.numeric(
+    dat[[
+      "unit_amount_decimal"
+    ]]
+  ))
+
+  dat[["created"]] <- lubridate::date(lubridate::as_datetime(dat[["created"]]))
+  dat
+}
